b2dc8c5c9819927682a8d28978cac8bc12d5b494
ultimatedev03 <ultimatedev2025@gmail.com>
2026-06-08 11:23:20 +0530

    Add footer social links

--- src/shared/components/layout/publicShell/footer/footerSocialLinks.js
+++ src/shared/components/layout/publicShell/footer/footerSocialLinks.js
@@ -1,8 +1,13 @@
 import { Briefcase, Mail, MessageCircle, Newspaper, Phone } from 'lucide-react';
+import { FaFacebook, FaInstagram, FaLinkedin, FaYoutube } from 'react-icons/fa';
 import { BLOG_BASE_URL } from '../../../../utils/externalLinks.js';
 import { HHH_JOBS_MASTER_CONTACT_NUMBERS, HHH_JOBS_SUPPORT_EMAIL } from '../../../../constants/contactInfo.js';
 
 export const footerSocialLinks = [
+  { label: 'LinkedIn', href: 'https://www.linkedin.com/company/hhh-jobs/', icon: FaLinkedin },
+  { label: 'Facebook', href: 'https://www.facebook.com/hhhjobs/', icon: FaFacebook },
+  { label: 'YouTube', href: 'https://www.youtube.com/@HHHJobs', icon: FaYoutube },
+  { label: 'Instagram', href: 'https://www.instagram.com/hhhjobs1/', icon: FaInstagram },
   { label: 'Support', href: '/contact-us', icon: MessageCircle },
   { label: 'Careers', href: '/careers', icon: Briefcase },
   { label: 'Updates', href: BLOG_BASE_URL, icon: Newspaper, newTab: false },
